15f1e9ff168f6131a64feb3b8df3676084343b00
ssumniee <suemink.dev@gmail.com>
2021-07-14 14:13:47 +0900

    add image-source-references

--- src/page/Gallery.js
+++ src/page/Gallery.js
@@ -4,13 +4,19 @@ function Gallery() {
   return (
     <div className="Gallery">
       <div className="thumbnail-area">
-        <div className="thumbnail-image"></div>
-        <div className="thumbnail-image"></div>
-        <div className="thumbnail-image"></div>
+        <div className="thumbnail-image">
+          <image src="./IMG/cat.jpeg" />
+        </div>
+        <div className="thumbnail-image">
+          <image src="./IMG/dog.jpeg" />
+        </div>
+        <div className="thumbnail-image">
+          <image src="./IMG/man.jpg" />
+        </div>
       </div>
       <div className="image-area">
         <div className="image-title"></div>
-        <image src="" />
+        <image src="./IMG/cat.jpeg" />
       </div>
     </div>
   );
